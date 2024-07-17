7839d3e957660c3de5d5a5f046961c8f3584e993
parneet-kaur14 <parneetkaur2003pk@gmail.com>
2024-07-16 20:43:39 -0400

    Commit

---
+++ ILFD.java
@@ -0,0 +1,15 @@
+public class ILFD {
+    public static void main(String[] args) {
+        
+        int a = -123;
+        long b = 9999999999999L;
+        float c = 2.9F;
+        double d = 534.5;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
