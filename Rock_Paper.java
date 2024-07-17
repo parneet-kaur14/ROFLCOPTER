a2c20bae86179330ad18b8b71d95e8d2dd336870
parneet-kaur14 <parneetkaur2003pk@gmail.com>
2024-07-17 12:20:55 -0400

    New commit

---
+++ Rock_Paper.java
@@ -0,0 +1,10 @@
+public class Rock_Paper {
+    public static void main(String[] args) {
+        
+        System.out.println("This is a rock, paper, scissors game!!");
+
+        System.out.println("Rules:");
+        System.out.println("");
+
+    }
+}
